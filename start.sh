55b682808a72f91a8406f953b2b1481c66fb743e
pik <pikdev.gopi@gmail.com>
2025-03-20 20:05:01 +0100

    start.sh update

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ set -e
 
 # Download model files from Google Drive
 echo "Downloading model files..."
-gdown https://drive.google.com/uc?id=$NLP_MODEL_FILE_ID -O models/nlp_model/
+gdown https://drive.google.com/uc?/export=download&id=$NLP_MODEL_FILE_ID -O models/nlp_model/
 
 # Run the Flask app
 echo "Starting the application..."
