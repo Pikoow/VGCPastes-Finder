dec55edb844fe41e6324336a1af44ded404881c8
pik <pikdev.gopi@gmail.com>
2025-03-20 11:53:54 +0100

    start update

--- start.sh
+++ start.sh
@@ -5,4 +5,4 @@ set -e
 
 # Run the Flask app
 echo "Starting the application..."
-gunicorn app:app --bind 0.0.0.0:${PORT:-5000} --workers 4
+gunicorn app.app:app --bind 0.0.0.0:${PORT:-5000} --workers 4
