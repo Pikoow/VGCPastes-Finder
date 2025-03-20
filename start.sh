26be988842c3cea10e62211c1182c27aa95d9d6c
pik <pikdev.gopi@gmail.com>
2025-03-20 20:20:13 +0100

    start.sh fix

--- start.sh
+++ start.sh
@@ -3,9 +3,11 @@
 # Exit immediately if any command fails
 set -e
 
+pip install gdown
+
 # Download model files from Google Drive
 echo "Downloading model files..."
-gdown https://drive.google.com/uc?/export=download&id=$NLP_MODEL_FILE_ID -O models/nlp_model/
+gdown "https://drive.google.com/uc?/export=download&id=$NLP_MODEL_FILE_ID" -O models/nlp_model/
 
 # Run the Flask app
 echo "Starting the application..."
