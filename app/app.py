249cb990405a39db02c5d1b61444fd8e44b6c050
pik <pikdev.gopi@gmail.com>
2025-03-20 13:45:49 +0100

    API Fix

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ import os
 
 app = Flask(__name__, template_folder="../templates", static_folder="../static")
 
-@app.route("/generate", methods=["POST"])
+@app.route("/api/generate", methods=["POST"])
 def generate():
     data = request.json
     instruction = data.get("instruction")
